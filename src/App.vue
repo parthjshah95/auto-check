71bb32e8588e7ec318642c2c0fb9eb4c3bcafce8
Parth Shah <parthjshah95@gmail.com>
2019-04-27 00:35:42 +0530

    sending base64 image

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,107 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view/> -->
+    <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand">Auto Assessor</a>
+    </nav>
+    <loading :active.sync="isLoading" 
+    :can-cancel="false" 
+    :is-full-page="fullPage"></loading>
+    <div class="card m-4 flex-row flex-wrap">
+      <div class="card-header w-100">
+        <h4 class="card-title">What is history?</h4>
+      </div>
+      <div class="card-block flex-col card-img">
+        <img v-bind:src="demo_image" class="card-img" alt="image not found">
+        <button class="w-100 btn btn-primary" v-on:click="test_image()">Submit image as answer</button>
+      </div>
+
+      <div class="card-body p-3 flex-fill">
+      </div>
+      
+    </div>
   </div>
 </template>
 
+<script>
+import demo_image from "@/assets/1_cropped.jpg";
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
+import VueBase64FileUpload from 'vue-base64-file-upload';
+
+
+export default {
+  name: "app",
+  components: {
+    Loading, VueBase64FileUpload
+  },
+  data(){
+    return {
+      demo_image : demo_image,
+      isLoading: false,
+      fullPage: true,
+      demo_answer: true
+    }
+  },
+  methods:{
+    test_image(){
+      this.isLoading = true
+      this.$image2base64(this.demo_image)
+      .then(
+          (response) => {
+              var base64Image = response
+              var body = {
+                "question":"demo",
+                "image": base64Image
+              }
+              this.$http.post("http://localhost:5000/assessImage", body).then(
+                function success(response){
+                  console.log(response)
+                  this.isLoading = false
+                },
+                function error(e){
+                  console.error(e)
+                  this.isLoading = false
+                }
+              )
+          }
+      )
+      .catch(
+          (error) => {
+              console.log(error);
+              this.isLoading = false
+          }
+      )
+    },
+    test(x){
+      console.log(x)
+      console.log(this.$refs.myFiles.files[0])
+      this.$http.get('http://localhost:5000/heartbeat').then(response => {
+        console.log(response.body)
+      });
+    }
+  }
+}
+</script>
+
+
 <style lang="scss">
+@import "../node_modules/bootstrap/scss/bootstrap.scss";
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+nav {
+  color: white;
+}
+.card-img{
+  width: 30rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
+import VueResource from 'vue-resource'
+import image2base64 from 'image-to-base64'
+Vue.use(VueResource)
 Vue.config.productionTip = false
+Object.defineProperty(Vue.prototype, '$image2base64', { value: image2base64 });
 
 new Vue({
   router,
