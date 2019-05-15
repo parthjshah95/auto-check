2a98401826844764ec2a78486b074aa31cb6c9a3
parshah <parth.shah@here.com>
2019-05-15 19:48:04 +0530

    0.0.2

--- deploy.py
+++ deploy.py
@@ -15,8 +15,16 @@ print(dash, "setting domain name to", domain, dash)
 with open("docs/CNAME", "w+") as c:
     c.write(domain)
 print("done")
+with open("version.txt") as v_file:
+    v_string_existing = v_file.read()
+    str_list = v_string_existing.split(".")
+    incremental_version = int(str_list[2])
+    incremental_version += 1
+    str_list[2] = str(incremental_version)
+    v_string_new = ".".join(str_list)
+print("version", v_string_new)
 print(dash, "deploying to github pages", dots, dash)
 os.system("git add .")
-os.system("git commit -m deployment")
+os.system("git commit -m " + v_string_new)
 os.system("git push")
 print(dash, "completed deployment process", dash)
